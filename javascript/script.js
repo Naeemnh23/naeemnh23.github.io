d34690497ec3abe63192156fb61121f94592156d
Naeem Hussain <naeemnh@outlook.com>
2018-08-11 18:36:46 +0400

    made adjustments
    added initial state for the sidebar

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	
+	$(".sidebar").addClass("state");
+	
 	$(".exit").click(function() {
 		$(".sidebar").removeClass("expand");
 		$(".section").removeClass("expand");
@@ -12,7 +14,8 @@ $(document).ready(function() {
 	$(".open").click(function() {
 		$(".item").removeClass("close");
 		$(".section").removeClass("close");
-		$(".sidebar").removeClass(".close");
+		$(".sidebar").removeClass("close");
+		$(".sidebar").removeClass("state");
 		$(".item").addClass("expand");
 		$(".section").addClass("expand");
 		$(".sidebar").addClass("expand");
