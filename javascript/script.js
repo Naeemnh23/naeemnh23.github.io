cb7996f147fcd4fa07f3f34810cd7bf4384c0d0b
Naeem Hussain <naeemnh@outlook.com>
2018-08-13 11:18:37 +0400

    removed js lines
    removed unneccesary lines in js

--- javascript/script.js
+++ javascript/script.js
@@ -4,20 +4,12 @@ $(document).ready(function() {
 	
 	$(".exit").click(function() {
 		$(".sidebar").removeClass("expand");
-		$(".section").removeClass("expand");
-		$(".item").removeClass("expand");
 		$(".sidebar").addClass("close");
-		$(".section").addClass("close");
-		$(".item").addClass("close");
 	});
 	
 	$(".open").click(function() {
-		$(".item").removeClass("close");
-		$(".section").removeClass("close");
 		$(".sidebar").removeClass("close");
 		$(".sidebar").removeClass("state");
-		$(".item").addClass("expand");
-		$(".section").addClass("expand");
 		$(".sidebar").addClass("expand");
 	});
 	
